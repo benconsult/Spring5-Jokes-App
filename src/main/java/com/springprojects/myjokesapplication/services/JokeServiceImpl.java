6240620b180475a02c4ee5a58d18a84b4dac4201
Aremu Benjamen <bendayo38@gmail.com>
2019-03-04 20:43:56 -0800

    Joke app java config. closes #1

--- src/main/java/com/springprojects/myjokesapplication/services/JokeServiceImpl.java
+++ src/main/java/com/springprojects/myjokesapplication/services/JokeServiceImpl.java
@@ -6,11 +6,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokeServiceImpl  implements JokeService{
     private final ChuckNorrisQuotes chuckNorrisQuotes;
+//generate a constructor param
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
 //spring does not know about the constructor , therefore it will be reused in that class and
     //not going to be created with every request
-    public JokeServiceImpl() {
+
+
+    //Refractoring
+    /*public JokeServiceImpl() {
         chuckNorrisQuotes = new ChuckNorrisQuotes();
-    }
+    }*/
 
 
     @Override
